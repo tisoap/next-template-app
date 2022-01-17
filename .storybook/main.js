9fb10b7b0351df93af594025e10e77390cd089fa
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-01-17 11:01:25 -0300

    update Storybook Chakra integration

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,23 +5,13 @@ module.exports = {
 		builder: 'webpack5'
 	},
 	stories: ['../components/**/*.stories.tsx'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@chakra-ui/storybook-addon'],
 	typescript: {
 		check: true,
-	},
-	features: {
-		emotionAlias: false,
-	},
-	refs: {
-		// Official Chakra UI page does not support Storybook Composition
-		// https://chakra-ui.netlify.app/
-		// https://github.com/chakra-ui/chakra-ui/issues/2263
-		'@chakra-ui/react': { disable: true },
 	},
 	webpackFinal: async (config) => {
 		// Enable absolute path imports
 		config.resolve.modules.push(path.resolve(__dirname, '../'))
-
 		return config
 	}
 }
