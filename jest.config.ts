7db37a4266882f196bab7bc7fd1dc02cc593a710
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-02-19 11:10:42 -0300

    Update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,15 +1,11 @@
-const config = {
-	testEnvironment: 'jsdom',
-	transform: {
-		// https://github.com/facebook/jest/issues/1468#issuecomment-361260279
-		'^.+\\.(j|t)sx?$': '<rootDir>/jestPreprocess.js'
-	},
-	moduleFileExtensions: ['js', 'ts', 'tsx'],
+import nextJest from 'next/jest'
+
+// https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
+const createJestConfig = nextJest({ dir: './' })
+
+const config = createJestConfig({
+	testEnvironment: 'jest-environment-jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-	moduleNameMapper: {
-		'\\.(css|less|scss)$': 'identity-obj-proxy'
-	},
 	collectCoverage: true,
 	collectCoverageFrom: [
 		'components/**/*.presentation.tsx',
@@ -23,14 +19,9 @@ const config = {
 			statements: 100
 		}
 	},
-	moduleDirectories: ['node_modules', __dirname],
+	moduleDirectories: ['node_modules', '<rootDir>/'],
 	resetMocks: true,
-	restoreMocks: true,
-	globals: {
-		'babel-jest': {
-			babelrcFile: '.storybook/.babelrc'
-		}
-	}
-}
+	restoreMocks: true
+})
 
 export default config
--- jestPreprocess.js
+++
@@ -1,6 +0,0 @@
-const babelOptions = {
-	presets: ['@babel/preset-typescript', 'next/babel']
-}
-
-// https://github.com/facebook/jest/issues/11444#issuecomment-847909076
-module.exports = require('babel-jest').default.createTransformer(babelOptions)
