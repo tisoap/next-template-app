45b339953e97cc7c3d91ec8abbbef4dbf6461754
Tiso Alvarez Puccinelli <28565117+tisoap@users.noreply.github.com>
2022-05-08 17:40:14 -0300

    Update deps

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,6 +2,7 @@ import { ColorModeScript } from '@chakra-ui/react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import type { DocumentContext, DocumentInitialProps } from 'next/document'
 
+// eslint-disable-next-line react-prefer-function-component/react-prefer-function-component
 class MyDocument extends Document {
 	static async getInitialProps(
 		ctx: DocumentContext
